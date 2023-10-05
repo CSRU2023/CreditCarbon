fc01307f8a35bbed5938234930d6a43046ae15c7
tam27936@gmail.com <tam27936@gmail.com>
2023-10-06 05:16:21 +0700

    Test Vite

---
+++ CreditCarbonWeb/src/views/TestView.vue
@@ -0,0 +1,22 @@
+
+<template>
+  <main>
+    <h1>Home</h1>
+    <button @click="counter++">Add 1</button>
+    <p>The button above has been clicked {{ counter }} times.</p>
+    <button @click="$router.push('/about')">About</button>
+  </main>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from 'vue'
+
+    export default defineComponent({
+        data() {
+            return {
+                counter: 0
+            }
+        }
+    })
+    
+</script>
