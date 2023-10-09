b5d2f5b79dbfa2feb7dc737eb4537bdcf2a4abb9
monniya.j <monniya.j@gmail.com>
2023-10-09 15:38:02 +0700

    [Edit]format code

--- CreditCarbonWeb/src/views/AboutView.vue
+++ CreditCarbonWeb/src/views/AboutView.vue
@@ -3,53 +3,31 @@
     <div class="col-12">
       <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
         <div class="carousel-indicators">
-          <button
-            type="button"
-            data-bs-target="#carouselExampleIndicators"
-            data-bs-slide-to="0"
-            class="active"
-            aria-current="true"
-            aria-label="Slide 1"
-          ></button>
-          <button
-            type="button"
-            data-bs-target="#carouselExampleIndicators"
-            data-bs-slide-to="1"
-            aria-label="Slide 2"
-          ></button>
-          <button
-            type="button"
-            data-bs-target="#carouselExampleIndicators"
-            data-bs-slide-to="2"
-            aria-label="Slide 3"
-          ></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
+            aria-current="true" aria-label="Slide 1"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+            aria-label="Slide 2"></button>
+          <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
+            aria-label="Slide 3"></button>
         </div>
         <div class="carousel-inner ">
           <div class="carousel-item active">
-            <img src="@/assets/img/car3.jpg" class="img-fluid" style="width: 100%;"/>
+            <img src="@/assets/img/car3.jpg" class="img-fluid" style="width: 100%;" />
           </div>
           <div class="carousel-item">
             <img src="@/assets/img/car4.jpg" class="img-fluid" style="width: 100%;" />
           </div>
           <div class="carousel-item">
-            <img src="@/assets/img/car1.jpg" class="img-fluid"  style="width: 100%;"/>
+            <img src="@/assets/img/car1.jpg" class="img-fluid" style="width: 100%;" />
           </div>
         </div>
-        <button
-          class="carousel-control-prev"
-          type="button"
-          data-bs-target="#carouselExampleIndicators"
-          data-bs-slide="prev"
-        >
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
+          data-bs-slide="prev">
           <span class="carousel-control-prev-icon" aria-hidden="true"></span>
           <span class="visually-hidden">Previous</span>
         </button>
-        <button
-          class="carousel-control-next"
-          type="button"
-          data-bs-target="#carouselExampleIndicators"
-          data-bs-slide="next"
-        >
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
+          data-bs-slide="next">
           <span class="carousel-control-next-icon" aria-hidden="true"></span>
           <span class="visually-hidden">Next</span>
         </button>
@@ -57,7 +35,7 @@
 
       <div>
         <h1 class="display-3">เกี่ยวกับ</h1>
-        <div class="card mb-3" style="max-width: 540px">
+        <div class="card mb-3" style="max-width: 100%">
           <div class="row g-0">
             <div class="col-md-4">
               <img src="..." class="img-fluid rounded-start" alt="..." />
@@ -88,5 +66,4 @@
   </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
--- CreditCarbonWeb/src/views/LoginView.vue
+++ CreditCarbonWeb/src/views/LoginView.vue
@@ -3,39 +3,29 @@
 
 
 <template>
-
-
-<div class = "container-sm">
-
-                  
-  
-  </div>
-
   <div class="container" style="
-    padding-right: 100px;
-    padding-left: 100px;">
-  <div class="row">
+    padding-right: 200px;
+    padding-left: 200px;">
+    <div class="row">
 
-<label for="login" style = "margin-bottom: 20px; text-align: center;
-">เข้าสู่ระบบ</label><br> 
+      <label for="login" style="margin-bottom: 20px; text-align: center;">เข้าสู่ระบบ</label><br>
 
-<input type="text" id="Username" name = "Username" class="form-control" placeholder="Username" style = "margin-bottom: 20px;" />
+      <input type="text" id="Username" name="Username" class="form-control" placeholder="Username"
+        style="margin-bottom: 20px;" />
 
-<input type="password" id="form2Example11" class="form-control" placeholder="password" style = "margin-bottom: 20px;"  />
+      <input type="password" id="form2Example11" class="form-control" placeholder="password"
+        style="margin-bottom: 20px;" />
 
- <div class="text-center pt-1 mb-5 pb-1">
-                    <button class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" type="button">Log
-                      in</button>
-                    <a class="text-muted" href="#!">Forgot password?</a>
-                  </div>
-                    
-                
-    
+      <div class="text-center pt-1 mb-5 pb-1">
+        <button class="btn btn-primary btn-block fa-lg gradient-custom-2 mb-3" type="button">Log
+          in</button>
+        <a class="text-muted" href="#!">Forgot password?</a>
+      </div>
+
+
+
+    </div>
   </div>
-</div>
-
-
-
 </template>
 
 
